11faecead51d91973c2136e83d4b5385d8b80a34
NielsMesure <niels.mesure@ecole-hexagone.com>
2023-01-27 11:45:04 +0100

    Commit carousel

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,15 +2,30 @@
 
 namespace App\Controller;
 
+use App\Entity\Job;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig');
+        $jobs=$this->entityManager->getRepository(Job::class)->findAll();
+
+        return $this->render('home/index.html.twig',[
+            'jobs'=>$jobs
+        ]);
+
     }
 }
